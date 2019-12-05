4a1235cd25f4dc5be51348ce0944cac26299c382
rhythm1705 <rhythm.goel.17@outlook.com>
2019-12-05 14:21:07 -0500

    Fix redux issue

--- client/src/store.js
+++ client/src/store.js
@@ -4,11 +4,13 @@ import rootReducer from "./reducers";
 const initialState = {};
 const middleware = [thunk];
 const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+	rootReducer,
+	initialState,
+	compose(
+		applyMiddleware(...middleware),
+		window.__REDUX_DEVTOOLS_EXTENSION__
+			? window.__REDUX_DEVTOOLS_EXTENSION__()
+			: compose
+	)
 );
 export default store;

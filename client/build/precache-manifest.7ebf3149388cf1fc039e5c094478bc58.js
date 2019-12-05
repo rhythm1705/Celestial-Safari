9d73b4b7bdf26842a355e6b035ef0dd4fc824bb3
rhythm1705 <rhythm.goel.17@outlook.com>
2019-12-05 10:58:39 -0500

    Deploy takeoff!

---
+++ client/build/precache-manifest.7ebf3149388cf1fc039e5c094478bc58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3d95bbc0a3840ab309555272cbb6c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14f47a7cf463d26bf25e",
+    "url": "/static/css/main.88e688f4.chunk.css"
+  },
+  {
+    "revision": "b09c9099f22d3bc27e0f",
+    "url": "/static/js/2.8f4a69a6.chunk.js"
+  },
+  {
+    "revision": "14f47a7cf463d26bf25e",
+    "url": "/static/js/main.e9849db5.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
